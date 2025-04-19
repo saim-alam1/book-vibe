0771727cdc6ab8409a72f2503645142da8a8f3a6
saim-alam1 <allbehassansaim@gmail.com>
2025-04-20 05:00:30 +0600

    params, useParams hook and fetching data dynamically with id

--- src/components/Pages/BookDetails/BookDetails.jsx
+++ src/components/Pages/BookDetails/BookDetails.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const BookDetails = () => {
+
+  const { id } = useParams();
+  const bookId = parseInt(id);
+  const data = useLoaderData();
+  const singleBook = data.find(book => book.bookId === bookId);
+  const { bookName, image } = singleBook;
+
   return (
-    <div>
-      <h1>Book Details</h1>
+    <div className='w-2/3 mx-auto'>
+      <img className='h-48' src={image} alt="" />
+      <h5>Book Name : {bookName}</h5>
+
+      <button className="btn btn-accent m-2">Read</button>
+      <button className="btn btn-info m-2">Wish List</button>
+
     </div>
   );
 };
--- src/components/Pages/Books/Books.jsx
+++ src/components/Pages/Books/Books.jsx
@@ -18,8 +18,14 @@ const Books = ({ data }) => {
       <h1 className='text-3xl text-center p-6'>Books</h1>
       <Suspense fallback={<span>Loading.....</span>}>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto gap-5 gap-x-13'>
-          {
+          {/* {
             data.map(singleBook => <Book key={singleBook.bookId} singleBook={singleBook}></Book>)
+          } */}
+
+          {
+            data.map(singleBook => (
+              <Book key={singleBook.bookId} singleBook={singleBook} />
+            ))
           }
         </div>
       </Suspense>
